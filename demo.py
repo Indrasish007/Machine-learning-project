b505a71ad87bb0df29da315edf0f877c0a9a6fb0
Indrasish <indrasishadhya770@gmail.com>
2024-02-29 03:23:09 +0530

    new

---
+++ demo.py
@@ -0,0 +1,18 @@
+planet1 ='earth'
+planet2 ='mars'
+planet2,planet1=planet1,planet2
+print("the first planet is ",planet1)
+print("the second planet is ",planet2)
+print("\n")
+planet1 ='indra'
+planet2 ='mars'
+planet2,planet1=planet1,planet2
+print("the first planet is ",planet1)
+print("the second planet is ",planet2)
+print("\n")
+planet1 ='ending'
+planet2 ='mars'
+planet2,planet1=planet1,planet2
+print("the first planet is ",planet1)
+print("the second planet is ",planet2)
+print("\n")
