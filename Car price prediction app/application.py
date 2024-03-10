c630f532114fa5c30ca8ef9611e6c6a31a9449ae
Indrasish <indrasishadhya770@gmail.com>
2024-03-10 08:18:07 +0530

    new

--- Car price prediction app/application.py
+++ Car price prediction app/application.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-if __name__ =="__main__":
-    app.run(debug=True , port=8000)
+    return "hello world"
+
+
+@app.route('/products')
+def products():
+    return "This is product page"
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
