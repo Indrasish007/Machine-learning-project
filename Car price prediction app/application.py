fd7c36d9fbdd3960714ce11b0e12134e91d3fa71
Indrasish <indrasishadhya770@gmail.com>
2024-03-14 03:08:22 +0530

    new

--- Car price prediction app/application.py
+++ Car price prediction app/application.py
@@ -10,8 +10,8 @@ def index():
     companies=sorted(car['company'].unique())
     car_models=sorted(car['name'].unique())
     year= sorted(car['year'].unique(),reverse=True)
-    fuel_type=car['fuel_type']
-    return render_template("index.html",companies=companies,car_models=car_models,year=year,fuel_type=fuel_type)
+    fuel_type=car['fuel_type'].unique()
+    return render_template("index.html",companies=companies,car_models=car_models,year=year,fuel_types=fuel_type)
 
 
 if __name__=="__main__":
