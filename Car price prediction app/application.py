64140274c55b9aa8204965de8f015234302068ce
Indrasish <indrasishadhya770@gmail.com>
2024-03-15 09:42:07 +0530

    new

--- Car price prediction app/application.py
+++ Car price prediction app/application.py
@@ -11,9 +11,25 @@ def index():
     car_models=sorted(car['name'].unique())
     year= sorted(car['year'].unique(),reverse=True)
     fuel_type=car['fuel_type'].unique()
+    
     return render_template("index.html",companies=companies,car_models=car_models,year=year,fuel_types=fuel_type)
 
 
 if __name__=="__main__":
     app.run(debug=True)
 
+# from flask import Flask
+# import pandas as pd
+# app = Flask(__name__)
+# car=pd.read_csv('refine_car.csv')
+
+# @app.route('/')
+# def hello_world():
+#     companies=sorted(car['company'].unique())
+#     car_models=sorted(car['name'].unique())
+#     year= sorted(car['year'].unique(),reverse=True)
+#     fuel_type=car['fuel_type'].unique()
+#     return str(len(car_models))
+
+# if __name__=="__main__":
+#      app.run(debug=True)
