fae835e5df94169cc2a80121bc46fed160c0c1eb
Simpl1fy <gourabdas07012004@gmail.com>
2024-04-15 00:18:19 +0530

    automated data ingestion, data transformation, model_training and fixed customexception

--- src/exception.py
+++ src/exception.py
@@ -1,17 +1,22 @@
-import os
 import sys
 from src.logger import logging
 
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(file_name, exc_tb.tb_lineno, str(error))
+def error_message_detail(error,error_detail=sys.exc_info()):
+    _,_,exc_tb=error_detail
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "unknown"
+        line_number = "unknown"
+        error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name,line_number,str(error))
 
     return error_message
 
 
-class CustomException:
-    def __init__(self, error_message, error_detail:sys):
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail=sys.exc_info()):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message, error_detail=error_detail)
 
